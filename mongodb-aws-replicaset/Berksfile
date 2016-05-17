2f27dae1820bb0c225e82f09804ec3035fd1b1c7
zhuangtim <zhuangtim@sina.cn>
2016-05-17 14:33:23 +0800

    add mongodb repicaset

---
+++ mongodb-aws-replicaset/Berksfile
@@ -0,0 +1,5 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook "mongodb", git: 'https://github.com/chuangtim/mongodb-cookbook.git'
---
+++ mongodb-aws-replicaset/metadata.rb
@@ -0,0 +1,7 @@
+name              'mongodb-aws-replicaset'
+maintainer        'edelight GmbH'
+maintainer_email  'markus.korn@edelight.de'
+license           'Apache 2.0'
+description       'Installs and configures demo'
+version           '0.16.3'
+
